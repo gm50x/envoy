e77373e80c00436d5b048eb02624324d05686474
Getúlio Magela Silva <getuliomagela@outlook.com>
2020-11-02 18:25:42 -0300

    fix: adjust init sql script

--- docker/docker-entrypoint-initdb.d/000.init.sql
+++ docker/docker-entrypoint-initdb.d/000.init.sql
@@ -4,6 +4,6 @@ set timezone to 'America/Sao_Paulo';
 
 create table envoy.user (
     username varchar(100) primary key,
-    name: varchar(300) not null,
-    password: varchar(300) not null
+    name varchar(300) not null,
+    password varchar(300) not null
 );
