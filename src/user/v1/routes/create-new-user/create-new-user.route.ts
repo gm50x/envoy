3033dbfa8f8fb044c8cb03c603492aab8f7088e0
Getúlio Magela Silva <getuliomagela@outlook.com>
2021-01-25 20:49:17 -0300

    feat: added api tokens for user creation route

--- src/user/v1/routes/create-new-user/create-new-user.route.ts
+++ src/user/v1/routes/create-new-user/create-new-user.route.ts
@@ -1,13 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Response } from '../../../../cross/v1';
 import { CreateNewUser } from '../../core';
 import { UserInput } from '../../domain';
 
 @Controller('users')
 @ApiTags('Users')
+@ApiBearerAuth()
+@UseGuards(AuthGuard('jwt'))
 export class CreateNewUserRoute {
   constructor(private readonly createNewUser: CreateNewUser) { }
   @Post()
+  @ApiResponse({ status: 200 })
+  @ApiResponse({ status: 422, type: Response })
+  @ApiResponse({ status: 500, type: Response })
   async activate(@Body() input: UserInput) {
     await this.createNewUser.activate(input);
   }
