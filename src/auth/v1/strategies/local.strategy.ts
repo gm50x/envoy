50320071576b2f65cb4aca9e0bd7f92b77fd098a
Getúlio Magela Silva <getuliomagela@outlook.com>
2021-01-25 14:42:47 -0300

    refactor: cleaned imports

--- src/auth/v1/strategies/local.strategy.ts
+++ src/auth/v1/strategies/local.strategy.ts
@@ -1,7 +1,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
-import { VerifyUser } from '../core/verify-user/verify-user.core';
+import { VerifyUser } from '../core';
 
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
