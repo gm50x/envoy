271bc5582c0e3a836d9f4428e608898099a8045d
Getúlio Magela Silva <getuliomagela@outlook.com>
2021-01-25 21:24:06 -0300

    fix: improper maped response

--- src/auth/v1/routes/verify-access-token/verify-access-token.route.ts
+++ src/auth/v1/routes/verify-access-token/verify-access-token.route.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Response } from 'src/cross/v1';
 
 @Controller('auth')
 @ApiTags('Auth')
